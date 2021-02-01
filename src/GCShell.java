430732369e1de2533ccced262147d279747a48f6
Dennis <dennisparkman12@gmail.com>
2021-02-01 13:35:34 -0500

    asd

---
+++ src/GCShell.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class GCShell
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> pastCommands = new ArrayList<>();
+        while(true)
+        {
+            
+        }
+    }
+}
